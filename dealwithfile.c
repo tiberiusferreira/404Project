757485696d4f67e875d489c86ea3a1c11ed17d26
tiberiusferreira <tiberiusferreira@gmail.com>
2014-10-23 12:19:22 -0200

    Initialise hex file implemented!!

--- dealwithfile.c
+++ dealwithfile.c
@@ -13,10 +13,11 @@ void removeComents(char *file_contents,int *size){
 }
 void compileProgram(char *file_contents,int *size){ //receives file contents and size of it
     char c;
-    char current_word[101];
+    char current_word[101], hex_file[100000]; //TODO get right hex file max size
     int current_source_line=1, current_source_line_word=0;
-    int current_hex_line=0,size_current_word=0;
+    int current_hex_pos=0,size_current_word=0;
     int local_size = *size,i,i_cur_word;
+    initialize_hex(hex_file);
     for(i=0;i<local_size;){ //go through all file
         c=file_contents[i]; //get each char from file
         for(;(file_contents[i]==' ' || file_contents[i]=='\n' )&& i<local_size;i++){ //jump white spaces and line breaks
@@ -37,11 +38,77 @@ void compileProgram(char *file_contents,int *size){ //receives file contents and
             current_word[i_cur_word]=file_contents[i];
         }
         size_current_word=i_cur_word;
-        printf("Tratando : %s , linha source = %d , %d palavra da linhas\n",current_word, current_source_line,current_source_line_word);
-        if(!strcmp(current_word,".word")){
-            printf("E word!\n");
-        }
+        //printf("Tratando : %s , linha source = %d , %d palavra da linhas\n",current_word, current_source_line,current_source_line_word);
+        //convert_word_to_instruction(current_word,size_current_word,current_source_line_word,hex_file,current_hex_pos);
+//        if(!strcmp(current_word,".word")){
+//            printf("E word!\n");
+//        }
     }
+}
+
+void initialize_hex(char *hex_file){
+    //17 chars
+    int i,line=0, line_as_hex=0,num_chars=0,i2;
+    //printf("%ld\n",strtol(,NULL,16));
+    //hex_file[i]=strtol(line,NULL,16);
+    char temp[100];
+    for(i=0;i<17407;){
+        num_chars=sprintf(temp,"%x",line);
+        for(i2=0;i2<num_chars;i2++){
+            hex_file[i+i2]=temp[i2];
+        }
+        i=i+i2;
+        while(i2<3){
+            hex_file[i]='0';
+            i++;
+            i2++;
+        }
+        hex_file[i]=' ';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]=' ';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]=' ';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]=' ';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]=' ';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]='L';
+        i++;
+        hex_file[i]='\n';
+        line++;
+        i++;
+    }
+    printf("%s",hex_file);
+}
+
+void convert_word_to_instruction(char *word, int word_size,int word_pos_in_line, char *hex_file, int hex_pos){
+    printf("Received word : %s, size %d, word_pos_in_line %d\n",word,word_size,word_pos_in_line);
+
 }
 
 char *fileToVector(FILE *source,int **size_contents){
--- dealwithfile.h
+++ dealwithfile.h
@@ -8,4 +8,6 @@ void removeComents(char *file_contents,int *size);
 char **fileToMatriz(FILE *arq, int **num_lines, int **num_chars_per_line);
 char *fileToVector(FILE *source, int **size_contents);
 void compileProgram(char *file_contents,int *size);
+void convert_word_to_instruction(char *word, int word_size, int word_pos_in_line, char *hex_file, int hex_pos);
+void initialize_hex(char *hex_file);
 #endif // DEALWITHFILE_H
