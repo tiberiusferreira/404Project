fc466391730c32d7d0e47aa8d54439c3b0536e66
tiberiusferreira <tiberiusferreira@gmail.com>
2014-10-23 21:33:10 -0200

    Corrected Init Function
    Previously it was printing using the format 100 for number 1, not it is
    the way it was supposed 1 = 001 etc.

--- dealwithfile.c
+++ dealwithfile.c
@@ -41,19 +41,25 @@ void getNextWord(char *current_word, int *current_source_line,int *current_sourc
 
 void initialize_hex(char *hex_file){
     //18 chars per line = 17 + \n
-    int i,line=0,num_chars=0,i2;
+    int i,line=0,num_chars=0,i2,i3;
     char temp[6];
     for(i=0;i<18414;){
         num_chars=sprintf(temp,"%x",line);
-        for(i2=0;i2<num_chars;i2++){
-            hex_file[i+i2]=temp[i2];
+        if(num_chars<3){
+
+            for(i3=num_chars;i3!=3;){
+                hex_file[i]='0';
+                i++;
+                i3++;
+            }
+
         }
-        i=i+i2;
-        while(i2<3){
-            hex_file[i]='0';
+        for(i2=0;i2<num_chars;){
+            hex_file[i]=temp[i2];
             i++;
             i2++;
         }
+
         hex_file[i]=' ';
         i++;
         hex_file[i]='L';
@@ -87,7 +93,55 @@ void initialize_hex(char *hex_file){
         i++;
     }
 }
+int hexchar_to_int(char *hex){
+    int number= (int) strtol(hex,NULL,0);
+    return number;
+}
 
+void write_to_hex(char *hex_file, char *memory_address_to_write, char *what_to_write,int write_to_dir){
+    char c;
+    int i_cur_word=0,i,current_source_line_word=0,current_source_line=0;
+    int target_line=hexchar_to_int(memory_address_to_write);
+    //printf("%s",hex_file);
+        if(target_line!=current_source_line){ //search for desired line
+        for(i=0;i<18414;(i)++){ //go until end of file
+            if(hex_file[i]=='\n'){ //if finds a line break increase source code line counter
+                (current_source_line)++;
+                if(target_line==current_source_line){
+                    i++; //end for pointing to first char of desired line
+                    break;
+                }
+            }
+            continue;
+        }
+        }
+        //at this point we expect to point to the first char of desired line
+        //if we are to write to left write_to_dir=0, if we are to write to right he is 1, if -1 we write to whole line
+        if(write_to_dir==-1){
+            i=i+4; //AAA DD DDD DD DDD is the format there, we were pointing to the first A, now to first D
+            hex_file[i]=what_to_write[0];
+            hex_file[i+1]=what_to_write[1];
+
+            hex_file[i+2]=' ';
+
+            hex_file[i+3]=what_to_write[2];
+            hex_file[i+4]=what_to_write[3];
+            hex_file[i+5]=what_to_write[4];
+
+            hex_file[i+6]=' ';
+
+            hex_file[i+7]=what_to_write[5];
+            hex_file[i+8]=what_to_write[6];
+
+            hex_file[i+9]=' ';
+
+
+            hex_file[i+10]=what_to_write[7];
+            hex_file[i+11]=what_to_write[8];
+            hex_file[i+12]=what_to_write[9];
+
+        }
+    }
 
 void convert_word_to_instruction(char *file_contents, int size_file_contents){
     char current_word[101], hex_file[18420];
@@ -100,15 +154,11 @@ void convert_word_to_instruction(char *file_contents, int size_file_contents){
     initialize_hex(hex_file);
     getNextWord(current_word,&current_source_line,&current_source_line_word,&size_current_word,&i,file_contents,size_file_contents);
     printf("Tratando : %s , linha source = %d , %d palavra da linhas\n",current_word, current_source_line,current_source_line_word);
-    getNextWord(current_word,&current_source_line,&current_source_line_word,&size_current_word,&i,file_contents,size_file_contents);
-    printf("Tratando : %s , linha source = %d , %d palavra da linhas\n",current_word, current_source_line,current_source_line_word);
-    getNextWord(current_word,&current_source_line,&current_source_line_word,&size_current_word,&i,file_contents,size_file_contents);
-    printf("Tratando : %s , linha source = %d , %d palavra da linhas\n",current_word, current_source_line,current_source_line_word);
-    getNextWord(current_word,&current_source_line,&current_source_line_word,&size_current_word,&i,file_contents,size_file_contents);
-    printf("Tratando : %s , linha source = %d , %d palavra da linhas\n",current_word, current_source_line,current_source_line_word);
 
+//    if(strcasecmp(current_word),'.word'){
+//        getNextWord(current_word,&current_source_line,&current_source_line_word,&size_current_word,&i,file_contents,size_file_contents);
 
-
+//    }
 }
 
 char *fileToVector(FILE *source,int **size_contents){
--- dealwithfile.h
+++ dealwithfile.h
@@ -9,6 +9,7 @@ char **fileToMatriz(FILE *arq, int **num_lines, int **num_chars_per_line);
 char *fileToVector(FILE *source, int **size_contents);
 void getNextWord(char *current_word, int *current_source_line,int *current_source_line_word, int *size_current_word, int *i, char *file_contents, int size_file_contents);
 void initialize_hex(char *hex_file);
-void write_to_hex(char *hex_file, int memory_address_to_write, char *what_to_write, int size);
+void write_to_hex(char *hex_file, char *memory_address_to_write, char *what_to_write, int write_to_dir);
 void convert_word_to_instruction(char *file_contents, int size_file_contents);
+int hexchar_to_int(char *hex);
 #endif // DEALWITHFILE_H
